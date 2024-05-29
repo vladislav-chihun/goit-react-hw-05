4dfb8cf25d386da7fe47c28ef147f8fb1b84051e
vladislav-chihun <vladikkosmo@gmail.com>
2024-05-29 20:06:16 +0200

    Update SearchField.jsx

--- src/components/SearchField/SearchField.jsx
+++ src/components/SearchField/SearchField.jsx
@@ -5,5 +5,29 @@ import { Form } from "react-router-dom";
 export default function SearchField() {
 
 
-    
+    function handleSearch(values,actions) {
+        if (values.query.trim() === "") return
+        onSearch(values.query);
+    actions.resetForm();
+    }
+
+
+
+    return (
+    <Formik initialValues={{ query: "" }} onSubmit={handleSearch}>
+      <Form >
+        <Field
+          
+          type="text"
+          name="query"
+          autoComplete="off"
+          autoFocus
+          placeholder="Search movie"
+        />
+        <button type="submit">
+          Search
+        </button>
+      </Form>
+    </Formik>
+  );
 }
