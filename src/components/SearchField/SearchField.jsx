bd365c82adf6440bb7fed749670f1728172f9893
vladislav-chihun <vladikkosmo@gmail.com>
2024-05-30 22:00:43 +0200

    more styles

--- src/components/SearchField/SearchField.jsx
+++ src/components/SearchField/SearchField.jsx
@@ -1,10 +1,10 @@
-
+import css from "./SearchField.module.css"
 export default function SearchField({ onSubmit }) {
     
     return (
         <form onSubmit={onSubmit} >
-            <input type="text" name='movieName' placeholder='Search'  />
-            <button type='submit' >Search</button>
+            <input type="text" name='movieName' placeholder='Search' className={css.input} />
+            <button type='submit' className={css.btn}>Search</button>
         </form>
     )
 }
